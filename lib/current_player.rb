e08f3d039a2320c29ac32199b9d5913f4bc68e75
Courtney Haltiwanger <chalt0319@gmail.com>
2018-05-31 14:58:36 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+  counter = 0 
+  board.each do |spot|
+    if spot == "X" || "O"
+      counter += 1 
+    end 
+  end 
+  return counter
+end 
+
+def current_player 
+  
